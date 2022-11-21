e086df0321c68835cda5503de84c981d2d5fab1a
Cactical <mljohnsonpuff@gmail.com>
2022-11-20 18:09:45 -0600

    Update to: Install shell script

--- install.sh
+++ install.sh
@@ -13,9 +13,6 @@ echo "#### Cleaning up... ####"
 rm -rf build
 rm -rf devenv.proj
 rm -rf CMakeLists.txt
-<<<<<<< HEAD
 sudo rm -rf /usr/local/bin/devenv
 sudo mv devenv /usr/local/bin/
-=======
->>>>>>> parent of 4a9a089 (Update to: Install shell script)
 echo "## Done! Run './devenv' to start DEN ##"
