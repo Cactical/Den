04a7f0bb068a0eaea9d5e671dfffa4e7414c8a28
Cactical <mljohnsonpuff@gmail.com>
2022-11-20 23:51:53 -0600

    Update to: Install shell script and README markdown file

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "## DEN install script (Version 0.1) ##"
 echo "Restoring build files"
 git restore .
 echo "Pulling from remote..."
-git pull --recurse-submodules
+git pull origin/dencode --recurse-submodules
 echo "#### Creating build dir... #### "
 cmake -S . -B build
 cd build 
