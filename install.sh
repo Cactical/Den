6bf1c1a4baa95ea1c982731da7350e98edb090a4
Cactical <mljohnsonpuff@gmail.com>
2022-11-20 18:08:15 -0600

    Revert "Update to: Install shell script"
    This reverts commit 4a9a08901120dc3adeff54cf92fde34d5d8ccd3e.

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "## DEN install script (Version 0.1) ##"
 echo "Pulling from remote..."
 git pull --recurse-submodules
@@ -14,6 +13,9 @@ echo "#### Cleaning up... ####"
 rm -rf build
 rm -rf devenv.proj
 rm -rf CMakeLists.txt
+<<<<<<< HEAD
 sudo rm -rf /usr/local/bin/devenv
 sudo mv devenv /usr/local/bin/
+=======
+>>>>>>> parent of 4a9a089 (Update to: Install shell script)
 echo "## Done! Run './devenv' to start DEN ##"
