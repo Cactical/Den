284bb5e0a43b610b7fe2475c612155523f1c0e3d
Cactical <mljohnsonpuff@gmail.com>
2022-11-23 23:00:04 -0600

    Addition of: Update shell script

---
+++ update.sh
@@ -0,0 +1,4 @@
+echo "## DEN update script (Version 0.1) ##"
+echo "#### This script updates the scripts (not DEN itself). ####"
+git pull https://github.com/Cactical/Den.git denscripts
+echo "## Done! Run 'install.sh' to install DEN. ##"
